c20c4ae9abef56ea6d9c1a224e3e95f6284b92d7
Farid Ardiansyah <faridardiansyah061@gmail.com>
2024-08-17 20:27:57 +0700

    Files upload

---
+++ app/dashboard/upload/page.tsx
@@ -0,0 +1,12 @@
+import FileUploader from "@/components/FileUploader";
+import React from "react";
+
+const FileUpload = () => {
+  return (
+    <div>
+      <FileUploader />
+    </div>
+  );
+};
+
+export default FileUpload;
